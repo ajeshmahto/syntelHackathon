344cd0543fae38f0394fb1e9ac5f9a0b2f9387be
ad99473 <ad99473@L35CMWZ1.us.ad.wellpoint.com>
2017-03-16 15:01:36 -0700

    first commit

---
+++ src/main/java/com/syntel/hackathon/HackthonMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.syntel.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackthonMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackthonMicroserviceApplication.class, args);
+	}
+}
